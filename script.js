a6e88753fa45649827c732e05814d11bfbe234cd
Victor <chenvictor247@gmail.com>
2022-01-23 04:17:07 -0500

    Button functions properly

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $('#button').onClick(function(){
     $('#target_element').addClass('animate_class_name');
 });
+function traincascade() {
+    var elem = document.getElementById("train1");
+        elem.classList.add("move");
+}
